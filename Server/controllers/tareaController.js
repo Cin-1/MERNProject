0161c72169785527a9c3e3e4ff83dee705cfc6f5
Cinthia Daira Pardos <cin@pop-os.localdomain>
2020-12-11 03:11:00 -0300

    257

---
+++ Server/controllers/tareaController.js
@@ -0,0 +1,99 @@
+const Tarea = require("../Models/Tarea");
+const Proyecto = require("../Models/Proyecto");
+const { validationResult } = require("express-validator");
+
+exports.crearTarea = async (req, res) => {
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+
+  try {
+    const { proyecto } = req.body;
+
+    const existeProyecto = await Proyecto.findById(proyecto);
+    if (!existeProyecto) {
+      return res.status(404).json({ msg: "Proyecto no encontrado" });
+    }
+    if (existeProyecto.creador.toString() !== req.usuario.id) {
+      return res.status(401).json({ msg: "No autorizado" });
+    }
+    const tarea = new Tarea(req.body);
+    await tarea.save();
+    res.json({ tarea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error");
+  }
+};
+
+exports.obtenerTareas = async (req, res) => {
+  try {
+    const { proyecto } = req.body;
+
+    const existeProyecto = await Proyecto.findById(proyecto);
+    if (!existeProyecto) {
+      return res.status(404).json({ msg: "Proyecto no encontrado" });
+    }
+    if (existeProyecto.creador.toString() !== req.usuario.id) {
+      return res.status(401).json({ msg: "No autorizado" });
+    }
+
+    const tareas = await Tarea.find({ proyecto });
+    res.json({ tareas });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error en el servidor");
+  }
+};
+exports.actualizarTarea = async (req, res) => {
+  try {
+    const { proyecto, nombre, estado } = req.body;
+    const { id } = req.params;
+    let tarea = await Tarea.findById(id);
+    if (!tarea) {
+      return res.status(404).json({ msg: "Tarea no encontrada" });
+    }
+    const existeProyecto = await Proyecto.findById(proyecto);
+
+    if (existeProyecto.creador.toString() !== req.usuario.id) {
+      return res.status(401).json({ msg: "No autorizado" });
+    }
+
+    const nuevaTarea = {};
+    if (nombre) {
+      nuevaTarea.nombre = nombre;
+    }
+    if (estado) {
+      nuevaTarea.estado = estado;
+    }
+    tarea = await Tarea.findOneAndUpdate({ _id: id }, nuevaTarea, {
+      new: true,
+    });
+    res.json({ tarea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error en el servidor");
+  }
+};
+
+exports.eliminarTarea = async (req, res) => {
+  try {
+    const { proyecto } = req.body;
+    const { id } = req.params;
+    let tarea = await Tarea.findById(id);
+    if (!tarea) {
+      return res.status(404).json({ msg: "Tarea no encontrada" });
+    }
+    const existeProyecto = await Proyecto.findById(proyecto);
+
+    if (existeProyecto.creador.toString() !== req.usuario.id) {
+      return res.status(401).json({ msg: "No autorizado" });
+    }
+    await Tarea.findOneAndRemove({ _id: id });
+    res.json({ msg: "Tarea eliminada" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error en el servidor");
+  }
+};
